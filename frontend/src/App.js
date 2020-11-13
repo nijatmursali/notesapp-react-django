d3b148a0cb199b3b0090a33bea4992f904de04c0
Nijat Mursali <nijatmursali@gmail.com>
2020-11-13 22:06:32 +0100

    added react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Button className="btn btn-secondary">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
