bb30072aa4bbb9839f852bb75497e0f3c421c0d0
Nijat Mursali <16105077+nijatmursali@users.noreply.github.com>
2020-12-02 17:46:46 +0100

    finished front-end

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,40 +1,39 @@
-import axios from "axios";
-
-const url = "http://127.0.0.1:8000/api/v1/notes/";
+const url = 'http://127.0.0.1:8000/api/v1/notes/';
 
 export const fetchNotes = async () => {
   return fetch(url, {})
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
+    .then(res => res.json())
+    .then(data => {
+      return data
     });
-};
+}
 
 export const fetchNote = (id) => {
-  return {
-    id: 123,
-    title: "Hello",
-    content: "Testing...",
-  };
-};
+  return fetch(`${url + id}`, {})
+    .then(res => res.json())
+    .then(data => {
+      return data;
+    });
+}
 
 export const addNote = (note) => {
   fetch(url, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(note),
+    body: JSON.stringify(note)
   })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  })
+
   return note;
-};
+}
 
 export const updateNote = (note) => {
-  console.log("Updating...");
-  console.log("Update a note with id ", note.id);
-};
+  console.log('Wer are updating...');
+  console.log('Update a note with id', note.id);
+}
